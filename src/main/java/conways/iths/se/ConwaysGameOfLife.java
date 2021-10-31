3b80dba536bb4adbd44acc8b379940eb8151a91d
Patricia Komstadius <patricia.komstadius@gmail.com>
2021-10-31 12:49:34 +0100

    class created + method for returning alive or dead cellstatus

---
+++ src/main/java/conways/iths/se/ConwaysGameOfLife.java
@@ -0,0 +1,11 @@
+package conways.iths.se;
+
+public class ConwaysGameOfLife {
+
+
+    public boolean isCurrentPositionAlive(int currentPositionValue) {
+
+        if(currentPositionValue == 1) return true;
+        else return false;
+    }
+}
