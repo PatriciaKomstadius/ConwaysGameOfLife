5be472d15d67495e6a6b65580fa7658b7557b62e
Patricia Komstadius <patricia.komstadius@gmail.com>
2021-10-31 13:33:07 +0100

    if-statement for alive cell with 2 alive neighbours returning three alive cells

--- src/main/java/conways/iths/se/ConwaysGameOfLife.java
+++ src/main/java/conways/iths/se/ConwaysGameOfLife.java
@@ -5,7 +5,7 @@ public class ConwaysGameOfLife {
 
     public boolean isCurrentPositionAlive(int currentPositionValue) {
 
-        if(currentPositionValue == 1) return true;
+        if (currentPositionValue == 1) return true;
         else return false;
     }
 
@@ -13,12 +13,12 @@ public class ConwaysGameOfLife {
 
         int[][] nextGenBoard = new int[board.length][board[0].length];
 
-        for (int row = 1; row < board.length -1; row++) {
-            for (int column = 1; column < board[0].length -1 ; column++) {
+        for (int row = 1; row < board.length - 1; row++) {
+            for (int column = 1; column < board[0].length - 1; column++) {
 
                 int neighboursAlive = 0;
 
-                for (int i = -1; i <=1; i++) {
+                for (int i = -1; i <= 1; i++) {
                     for (int k = -1; k <= 1; k++) {
                         neighboursAlive += board[row + i][column + k];
                     }
@@ -28,6 +28,7 @@ public class ConwaysGameOfLife {
                 boolean alive = isCurrentPositionAlive(board[row][column]);
 
                 if (alive && neighboursAlive <= 1) nextGenBoard[row][column] = 0;
+                else if (alive && neighboursAlive == 2) nextGenBoard[row][column] = 1;
                 else nextGenBoard[row][column] = board[row][column];
             }
         }
