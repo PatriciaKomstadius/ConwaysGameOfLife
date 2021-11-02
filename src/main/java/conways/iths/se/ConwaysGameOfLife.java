89d28fc965b1b323c71a1b6f9ef29915be912c57
Patricia Komstadius <patricia.komstadius@gmail.com>
2021-11-02 09:03:50 +0100

    Extracting magic numbers in method to field constants

--- src/main/java/conways/iths/se/ConwaysGameOfLife.java
+++ src/main/java/conways/iths/se/ConwaysGameOfLife.java
@@ -1,8 +1,11 @@
 package conways.iths.se;
 
 public class ConwaysGameOfLife {
-    
-    
+
+
+    private final int ONE_ALIVE_NEIGHBOUR = 1;
+    private final int THREE_ALIVE_NEIGHBOURS = 3;
+
     public boolean isCurrentPositionAlive(int currentPositionValue) {
         return currentPositionValue == 1;
     }
@@ -22,9 +25,10 @@ public class ConwaysGameOfLife {
 
                 boolean alive = isCurrentPositionAlive(board[row][column]);
 
-                if (alive && neighboursAlive <= 1 || alive && neighboursAlive > 3) {
+                //extracting magic numbers to field constants
+                if (alive && neighboursAlive <= ONE_ALIVE_NEIGHBOUR || alive && neighboursAlive > THREE_ALIVE_NEIGHBOURS) {
                     nextGenBoard[row][column] = 0;
-                } else if (!alive && neighboursAlive == 3) {
+                } else if (!alive && neighboursAlive == THREE_ALIVE_NEIGHBOURS) {
                     nextGenBoard[row][column] = 1;
                 } else {
                     nextGenBoard[row][column] = board[row][column];
