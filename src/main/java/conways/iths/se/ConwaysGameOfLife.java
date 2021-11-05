db260a1c80f04076f428e59812a645b48c79ba52
Patricia Komstadius <patricia.komstadius@gmail.com>
2021-11-05 08:27:04 +0100

    method for printing boards + method for first boardinput

--- src/main/java/conways/iths/se/ConwaysGameOfLife.java
+++ src/main/java/conways/iths/se/ConwaysGameOfLife.java
@@ -1,16 +1,60 @@
 package conways.iths.se;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class ConwaysGameOfLife {
 
-    private final int ONE_ALIVE_NEIGHBOUR = 1;
-    private final int THREE_ALIVE_NEIGHBOURS = 3;
     private int[][] nextGenBoard;
     private int row;
     private int column;
+    private static final ConwaysGameOfLife gol = new ConwaysGameOfLife();
+
+
+    public static void main(String[] args) {
+
+        System.out.println("----------- FIRST GENERATION ----------");
+
+        gol.printGeneration(gol.board());
+
+        System.out.println("----------- SECOND GENERATION ----------");
+
+        gol.printGeneration(gol.nextGeneration(gol.board()));
+
+    }
+
+    public int[][] board() {
+
+        int[][] initialBoard = {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+        return initialBoard;
+    }
 
     public State isCurrentPositionAlive(int currentPositionValue) {
         if (currentPositionValue == 1) return State.ALIVE;
         else return State.DEAD;
+    }
+
+    private void calculateNextGeneration(int[][] board, int neighboursAlive) {
+
+        State state = isCurrentPositionAlive(board[row][column]);
+
+        int THREE_ALIVE_NEIGHBOURS = 3;
+        int ONE_ALIVE_NEIGHBOUR = 1;
+
+        if (state == State.ALIVE && neighboursAlive <= ONE_ALIVE_NEIGHBOUR || state == State.ALIVE && neighboursAlive > THREE_ALIVE_NEIGHBOURS) {
+            nextGenBoard[row][column] = 0;
+        } else if (state == State.DEAD && neighboursAlive == THREE_ALIVE_NEIGHBOURS) {
+            nextGenBoard[row][column] = 1;
+        } else {
+            nextGenBoard[row][column] = board[row][column];
+        }
     }
 
     public int[][] nextGeneration(int[][] board) {
@@ -32,17 +76,30 @@ public class ConwaysGameOfLife {
         return nextGenBoard;
     }
 
-    private void calculateNextGeneration(int[][] board, int neighboursAlive) {
+    public String printGeneration(int [][] printThisBoard) {
 
-        State state = isCurrentPositionAlive(board[row][column]);
+        String printBoard = Arrays
+                .stream(printThisBoard)
+                .map(Arrays::toString)
+                .collect(Collectors.joining(System.lineSeparator()));
 
-        if (state == State.ALIVE && neighboursAlive <= ONE_ALIVE_NEIGHBOUR || state == State.ALIVE && neighboursAlive > THREE_ALIVE_NEIGHBOURS) {
-            nextGenBoard[row][column] = 0;
-        } else if (state == State.DEAD && neighboursAlive == THREE_ALIVE_NEIGHBOURS) {
-            nextGenBoard[row][column] = 1;
-        } else {
-            nextGenBoard[row][column] = board[row][column];
+        if (printBoard.contains("1"))
+            printBoard = printBoard.replace("1", "*");
+
+
+        if (printBoard.contains("0"))
+            printBoard = printBoard.replace("0", ".");
+
+
+        if (printBoard.contains(",") || printBoard.contains("[") || printBoard.contains("]")) {
+            printBoard = printBoard.replace(",", " ");
+            printBoard = printBoard.replace("[", " ");
+            printBoard = printBoard.replace("]", " ");
         }
+
+        System.out.println(printBoard);
+
+        return printBoard;
     }
 
 }
